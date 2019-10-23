591a5a714d07aaeef8970f7c17b1956f060c47f4
Ian Baylis <ibaylis@USMDLT824.local>
2019-10-23 18:02:26 -0400

    Added api to f2f project with env

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -6,13 +6,41 @@ import { recipeData } from '../data/tempList';
 class Recipes extends Component {
     constructor(props) {
         super(props);
-        
+        this.getRecipes = this.getRecipes.bind(this);
     };
 
     state = {
         recipes: recipeData,
-        search: ""
+        search: "",
+        url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+        base_url: `https://www.food2fork.com/api/search?key=${process.env.REACT_APP_API_KEY}`,
+        query: '&q=',
+        error: ''
     };
+
+    async getRecipes() {
+        try {
+            const data = await fetch(this.state.url);
+            const jsonData = await data.json();
+            if (jsonData.recipes.length === 0) {
+                this.setState({
+                    error: "Sorry but the gnombs did not return any recipes, please try again or press the search icon for the most popular recipes"
+                });
+            } else {
+                this.setState({
+                    recipes: jsonData.recipes,
+                    error: ""
+                });
+                console.log(jsonData);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    componentDidMount() {
+        this.getRecipes();
+    }
 
     handleChange = (e) => {
         this.setState({
@@ -22,7 +50,12 @@ class Recipes extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-    }
+        const {base_url, query, search} = this.state;
+        this.setState({
+            url: `${base_url}${query}${search}`,
+            search: ''
+        }, ()=>this.getRecipes())
+    };
 
     render() {
         return (
@@ -32,9 +65,19 @@ class Recipes extends Component {
                     handleChange={this.handleChange}
                     handleSubmit={this.handleSubmit}
                 />
-                <RecipeList 
-                    recipes={this.state.recipes}
-                />
+                {this.state.error ? (
+                    <section>
+                        <div className="row">
+                            <div className="col">
+                                <h2 className="text-orange text-center text-uppercase">
+                                    {this.state.error}
+                                </h2>
+                            </div>
+                        </div>
+                    </section>
+                ) : (
+                    <RecipeList recipes={this.state.recipes} />
+                )}
             </>
         )
     }
